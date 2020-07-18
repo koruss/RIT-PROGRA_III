de892bf751e344b9a32ce377527ea3d026282c03
Anner <anner.calvo@gmail.com>
2020-07-17 18:55:51 -0600

    Add stopwords

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-print ("Hola mundo");
+import re
+import math
+
+def readText():
+    f = open ('C:/Users/admin/Desktop/reut2-001.sgm','r');
+    mensaje="";
+    for line in f.readlines():
+        print(line);
+    f.close();
+
+def readStopWords(dictStopWords):
+    f = open ('C:/Users/admin/Desktop/Rit3/RIT-PROGRA_III/stopwords.txt','r');
+    message = f.read();
+
+    patternStopWords = re.compile(r"([a-z']+)");
+    listStopWords = patternStopWords.findall(message);
+    for i in listStopWords:
+        dictStopWords[i]=i;
+    return dictStopWords;
+
+def removeComma(Number):
+    new = re.sub('(?<=\d),(?=\d)', '', Number);
+    return new;
+
+def entropiaClases():
+    
+
+
+
+def main():
+    dictStopWords = {};
+    # readStopWords(dictStopWords);
+    number = "9,1651,6168,3216,3158,3216,316,4685.21651654"
+    number1="7,030,000 asfd sfada,sfd a asdfas qwer 4654 13213 5613.16354 qwer w654 4re,wq6r ewqre,wq asdf 654 321 1521.146 adfas 4654,79865.465 64,64968,65321.163"
+    new=removeComma(number1);
+    print(new);
+
+
+
+
+
+
+    # print("Ingrese la ruta del archivo que contiene los artículos noticiosos");
+    # path = input();
+    # print("Ingrese la cantidad de mejores términos que serán escogidos (numMejores)");
+    # numMejores = input();
+    # print("Ingrese la cantidad mínima de documentos por clase (minNc)");
+    # minNc = input();
+	# print("Ingrese la cantidad mínima de documentos por término (minNi)");
+    # minNi = input();
+	# print("Ingrese un prefijo para usar en los nombres de los archivos generados");
+    # pref = input();
+
+main();
+
