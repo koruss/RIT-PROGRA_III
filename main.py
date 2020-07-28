19feb6b0707b3dfa2068452bdfb5805e901cd434
Anner <anner.calvo@gmail.com>
2020-07-27 20:55:33 -0600

    Some problems fixed

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import re
 import math
 from os import remove
 
+def writeClass(dictClass, minNc):
+    g = open('clases.txt','w');
+    h = open('docs.txt','w');
+    for i in dictClass:
+        if dictClass.get(i)[0] > minNc and dictClass.get(i)[2] != []:
+            h.write(i + ':' + str(dictClass.get(i)[2]) + '\n')
+        if dictClass.get(i)[0] > minNc:
+            g.write(i + ':' + str(dictClass.get(i)[0]) + '\n');
+    g.close();
+
+def writeDicc(dictWords, minNi):    
+    f = open('dicc.txt','w');
+    for i in dictWords:
+        f.write(i + ':' + str(dictWords[i][0]) + '\n');
+    f.close();
+
 
 def readStopWords(dictStopWords):
     f = open ('stopwords.txt','r');
@@ -38,7 +54,7 @@ def totalEntropy(dictClass, numDocs):
 
 
 
-def termEntropy(dictClass, dictWords, numDocs):
+def termEntropy(dictClass, dictWords, numDocs, minNi):
     # por cada uno de los elementos del dictWords
     # hacer las siguientes operaciones
     # 1- ni*log(ni,2)
@@ -51,33 +67,29 @@ def termEntropy(dictClass, dictWords, numDocs):
     # 
     #
     # dictWords[key]=[ni, cont, {classWord:1}, result, IG];
-    # for i in dictWords['a'][1]:
-    #     print(dictWords['a'][1][i]);
-    print(dictClass.get('coffee'))
-    print(dictWords.get('said'))
     for key in dictWords:
         ni=dictWords[key][0]
-        Nmni = (numDocs - ni)
+        if ni > minNi:
+            Nmni = (numDocs - ni)
 
-        op1=(ni)*(math.log2(ni));
-        op2=(Nmni)*(math.log2(Nmni));
-        op3=0;
-        op4=0;
-        for i in dictWords.get(key)[2]:
-            nik=(dictWords.get(key)[2][i])
-            op3+=(nik*(math.log2(nik)))
+            op1=(ni)*(math.log2(ni));
+            op2=(Nmni)*(math.log2(Nmni));
+            op3=0;
+            op4=0;
+            for i in dictWords.get(key)[2]:
+                nik=(dictWords.get(key)[2][i])
+                op3+=(nik*(math.log2(nik)))
 
-        for i in dictWords.get(key)[2]:
-            nik=(dictWords.get(key)[2][i])
-            nc=(dictClass.get(i)[0])
-            ncmnik=(nc-nik)
-            if ncmnik>0:
-                print(key+': nc='+str(nc)+', nik='+str(nik)+', ncmnik='+str(ncmnik))
-                op4+=((ncmnik)*(math.log2(ncmnik)))
+            for i in dictWords.get(key)[2]:
+                nik=(dictWords.get(key)[2][i])
+                nc=(dictClass.get(i)[0])
+                ncmnik=(nc-nik)
+                if ncmnik>0:
+                    op4+=((ncmnik)*(math.log2(ncmnik)))
 
-        num=(op1+op2-op3-op4)
-        result=num/numDocs;
-        dictWords[key][3]=result;
+            num=(op1+op2-op3-op4)
+            result=num/numDocs;
+            dictWords[key][3]=result;
 
 
 
@@ -112,7 +124,7 @@ def readBody(toGet):
         return content.text
 
 
-def getData(dictClass, dictWords):
+def getClass(dictClass):
     # dictClass={key:[nc, result, [NEWID]]}
     f = open('temp.txt','r');
     regex = r".*TOPICS=\"YES\".*NEWID=\"(\d+).*"
@@ -128,49 +140,46 @@ def getData(dictClass, dictWords):
         dictClass[topic][2].append(matches.group(1));
         
 
-def getWords(dictClass, dictWords, dictStopWords, cont):
-
-    # dictWords = {key: ni, cont, [class, class]}
+def getWords(dictClass, dictWords, dictStopWords, cont, minNc):
+    # dictClass={key:[nc, result, [NEWID]]}
+    # dictWords = {key: ni, cont, [class, class], result, IG}
     text='';
     text=readBody('BODY');
     classWord=readTopic('TOPICS');
-    if text != None:
-        wordsComma = re.sub('(?<=\d),(?=\d)', '', text);
-        wordsLower = wordsComma.lower();
-        wordsSplit = wordsLower.split();
-        words = removeStopWords(wordsSplit, dictStopWords);
-        for i in words:
-            if (dictWords.get(i) and cont != dictWords[i][1]):
-                dictWords[i][0]+=1;
-                dictWords[i][1] = cont; 
-                if (dictWords[i][2].get(classWord)):
-                    dictWords[i][2][classWord] += 1;
-                else:
-                    dictWords[i][2][classWord] = 1;
-            elif (dictWords.get(i)==None):
-                dictWords[i]=[1, cont, {classWord:1}, 0, 0];
+    if dictClass.get(classWord)[0] > minNc and dictClass.get(classWord)[2] != []:
+        if text != None:
+            wordsComma = re.sub('(?<=\d),(?=\d)', '', text);
+            wordsLower = wordsComma.lower();
+            wordsSplit = wordsLower.split();
+            words = removeStopWords(wordsSplit, dictStopWords);
+            for i in words:
+                if (dictWords.get(i) and cont != dictWords[i][1]):
+                    dictWords[i][0]+=1;
+                    dictWords[i][1] = cont; 
+                    if (dictWords[i][2].get(classWord)):
+                        dictWords[i][2][classWord] += 1;
+                    else:
+                        dictWords[i][2][classWord] = 1;
+                elif (dictWords.get(i)==None):
+                    dictWords[i]=[1, cont, {classWord:1}, 0, 0];
                 
 
-    # print(dictWords);
 
-
-
-def createTempFile(document, dictClass, dictWords, dictStopWords, cont):
+def createTempFile(document, dictClass, dictWords, dictStopWords, cont, first, minNc):
     f = open ('temp.txt','w');
     f.write(document);
     f.close();
-    getData(dictClass, dictWords);
-    getWords(dictClass, dictWords, dictStopWords, cont);
+    if first == 0:
+        getClass(dictClass);
+    else:
+        getWords(dictClass, dictWords, dictStopWords, cont, minNc);
     
 
 
 
 
-def readText(dictClass, dictWords, dictStopWords):
+def readText(dictClass, dictWords, dictStopWords, first, minNc):
     f = open ('reut2-001.sgm','r');
-    g = open('clases.txt','w');
-    h = open('docs.txt','w');
-    i = open('dicc.txt','w');
     document="";
     max=10000000;
     cont=1;
@@ -178,7 +187,7 @@ def readText(dictClass, dictWords, dictStopWords):
     for line in f.readlines():
         matches = re.match(regex, line, re.MULTILINE);
         if matches and cont<=max:
-            createTempFile(document, dictClass, dictWords, dictStopWords, cont);
+            createTempFile(document, dictClass, dictWords, dictStopWords, cont, first, minNc);
             cont+=1;
             document=""
             # print("Aquí termina --------------")
@@ -189,6 +198,19 @@ def readText(dictClass, dictWords, dictStopWords):
 
 
 def main():
+    # print("Ingrese la ruta del archivo que contiene los artículos");
+    # path = input();
+    # print("Ingrese la cantidad mínima de documentos por clase (minNc)");
+    # minNc = input();
+	# print("Ingrese la cantidad mínima de documentos por término (minNi)");
+    # minNi = input();
+    # print("Ingrese la cantidad de mejores términos que serán escogidos (numMejores)");
+    # numMejores = input();
+	# print("Ingrese un prefijo para usar en los nombres de los archivos generados");
+    # pref = input();
+
+    minNc=6;
+    minNi=5;
     dictStopWords = {};
     # dictClass={key:[nc, result, [NEWID]]}
     # el dictClass necesita el nombre como key, el número de veces que aparece esa clase, y result(el resultado de p*log(p,2)) como espacios en valor,
@@ -204,25 +226,30 @@ def main():
     # 
     dictWords = {};
     readStopWords(dictStopWords);
-    numDocs=readText(dictClass, dictWords, dictStopWords);
- 
-    generalEntropy=totalEntropy(dictClass, numDocs);
-    termEntropy(dictClass, dictWords, numDocs);
+    first = [0,1]
 
-    IG(dictWords, generalEntropy);
-
+    for i in first:
+        if i == 0:
+            # Se sacan primero las clases
+            numDocs=readText(dictClass, dictWords, dictStopWords, i, minNc);
+        else:
+            numDocs=readText(dictClass, dictWords, dictStopWords, i, minNc);
     
 
-    # print("Ingrese la ruta del archivo que contiene los artículos noticiosos");
-    # path = input();
-    # print("Ingrese la cantidad de mejores términos que serán escogidos (numMejores)");
-    # numMejores = input();
-    # print("Ingrese la cantidad mínima de documentos por clase (minNc)");
-    # minNc = input();
-	# print("Ingrese la cantidad mínima de documentos por término (minNi)");
-    # minNi = input();
-	# print("Ingrese un prefijo para usar en los nombres de los archivos generados");
-    # pref = input();
+    # generalEntropy=totalEntropy(dictClass, numDocs);
+    # termEntropy(dictClass, dictWords, numDocs, minNi);
+
+    # IG(dictWords, generalEntropy);
+
+    # print(dictClass)
+    # print(dictWords)
+
+
+    writeClass(dictClass, minNc);
+    writeDicc(words, minNi);
+    
+
+
 
 
 main();
